77f8872c816fe324f0c7847886481d60e8068f46
Jesse Shawl <jesse@jshawl.com>
2014-12-25 21:15:53 -0600

    get child state

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -5,20 +5,26 @@ var TicTacToeBoard = React.createClass({
   changeTurn: function(){
     this.setState({
       turn: this.state.turn == "x" ? "o" : "x"
+    }, function(){
+      for( ref in this.refs ){
+	console.log( this.refs[ref.toString()].state.turn);
+      }
     });
   },
   render: function(){
     return (
       <div className="ticTacToeBoard" >
-        <TicTacToeSquare turn={this.state.turn} changeTurn={this.changeTurn}/>
-        <TicTacToeSquare turn={this.state.turn} changeTurn={this.changeTurn}/>
-        <TicTacToeSquare turn={this.state.turn} changeTurn={this.changeTurn}/>
-        <TicTacToeSquare turn={this.state.turn} changeTurn={this.changeTurn}/>
-        <TicTacToeSquare turn={this.state.turn} changeTurn={this.changeTurn}/>
-        <TicTacToeSquare turn={this.state.turn} changeTurn={this.changeTurn}/>
-        <TicTacToeSquare turn={this.state.turn} changeTurn={this.changeTurn}/>
-        <TicTacToeSquare turn={this.state.turn} changeTurn={this.changeTurn}/>
-        <TicTacToeSquare turn={this.state.turn} changeTurn={this.changeTurn}/>
+        <TicTacToeSquare ref="0" turn={this.state.turn} changeTurn={this.changeTurn}/>
+        <TicTacToeSquare ref="1" turn={this.state.turn} changeTurn={this.changeTurn}/>
+        <TicTacToeSquare ref="2" turn={this.state.turn} changeTurn={this.changeTurn}/>
+
+        <TicTacToeSquare ref="3" turn={this.state.turn} changeTurn={this.changeTurn}/>
+        <TicTacToeSquare ref="4" turn={this.state.turn} changeTurn={this.changeTurn}/>
+        <TicTacToeSquare ref="5" turn={this.state.turn} changeTurn={this.changeTurn}/>
+
+        <TicTacToeSquare ref="6" turn={this.state.turn} changeTurn={this.changeTurn}/>
+        <TicTacToeSquare ref="7" turn={this.state.turn} changeTurn={this.changeTurn}/>
+        <TicTacToeSquare ref="8" turn={this.state.turn} changeTurn={this.changeTurn}/>
       </div>
     );	  
   }
@@ -30,11 +36,11 @@ var TicTacToeSquare = React.createClass({
   },
   handleClick: function(){
     if( !this.state.checked ){
-      this.props.changeTurn(this.props.key, this.props.turn);
       this.setState({ 
 	checked: true,
 	turn: this.props.turn
       });
+      this.props.changeTurn(this.props.key, this.props.turn);
     }
   },
   render: function(){
